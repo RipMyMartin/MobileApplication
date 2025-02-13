784585c4db1568a4fc43adf2c97ff919621c553c
opilane <opilane@HP-CZC2349HTY>
2025-02-13 13:21:19 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace MobileApplication
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var valgus_btn = new Button
+            {
+                Text = "Valgusfoor",
+                BackgroundColor = Color.FromRgb(255, 255, 255),
+            };
+
+            valgus_btn.Clicked += Valgus_btn_Clicked;
+
+            Content = new StackLayout
+            {
+                Margin = 20,
+                Children =
+                {
+                    valgus_btn
+                }
+            };
+        }
+
+        private async void Valgus_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Valgusfoorpage());
+        }
+    }
+}

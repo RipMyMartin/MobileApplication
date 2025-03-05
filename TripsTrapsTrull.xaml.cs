626a547dfceb5a24e97d23aaa4f2a457140e66c5
ripmymartin <martinsild.mr@gmail.com>
2025-03-05 11:15:46 +0200

    commit

--- TripsTrapsTrull.xaml.cs
+++ TripsTrapsTrull.xaml.cs
@@ -1,244 +1,149 @@
 ﻿using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Controls.PlatformConfiguration.TizenSpecific;
 
 namespace MobileApplication;
 
 public partial class TripsTrapsTrull : ContentPage
 {
-    Image img;
-    Grid field;
-    Button turnbtn, clearbtn;
-    Label CrossOrZero;
-    static int player;
-    int[,] WinOrLose = new int[3, 3];
-    static int victor = 0;
+    private string[] board;
+    private bool isPlayerTurn;
     public TripsTrapsTrull(int k)
     {
-        Playfield();
-        BackgroundColor = Color.FromRgb(241, 140, 78);
-
-        turnbtn = new Button()
-        {
-            Text = "Turn Decider",
-            HorizontalOptions = LayoutOptions.FillAndExpand,
-            VerticalOptions = LayoutOptions.EndAndExpand,
-        };
-        turnbtn.Clicked += turnbtnOnClicked;
-
-        clearbtn = new Button()
-        {
-            Text = "Restart",
-            HorizontalOptions = LayoutOptions.FillAndExpand,
-            VerticalOptions = LayoutOptions.EndAndExpand,
-        };
-        clearbtn.Clicked += clearbtnOnClicked;
-
-
-        StackLayout BtnLayout = new StackLayout()
-        {
-            Orientation = StackOrientation.Horizontal,
-            Children = { clearbtn, turnbtn }
-        };
-
-        CrossOrZero = new Label()
-        {
-            TextColor = Color.FromRgb(255,255, 255),
-        };
-
-        StackLayout MainLayout = new StackLayout()
-        {
-            Children = { field, BtnLayout, CrossOrZero }
-        };
-
-        Content = MainLayout;
+        InitializeComponent();
+        NewGame();
     }
-    private void Playfield()
+
+    private void NewGame()
     {
-        field = new Grid()
+        board = new string[9];
+        isPlayerTurn = true;
+        UpdateBoard();
+    }
+
+    private void UpdateBoard()
+    {
+        for (int i = 0; i < 9; i++)
         {
-            HeightRequest = 400
-        };
-        for (int i = 0; i < 3; i++)
-        {
-            field.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-            field.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            var button = (Button)FindByName($"Button{i}");
+            button.Text = board[i] ?? string.Empty;
+            button.IsEnabled = string.IsNullOrEmpty(board[i]);
         }
 
-        for (int i = 0; i < 3; i++)
+        var winner = CheckWinner();
+        if (winner != null)
         {
-            for (int j = 0; j < 3; j++)
+            DisplayAlert("Game Over", winner == "Draw" ? "It's a draw!" : $"{winner} wins!", "New Game");
+            NewGame();
+        }
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        var button = (Button)sender;
+        int index = int.Parse(button.ClassId);
+
+        if (isPlayerTurn)
+        {
+            board[index] = "X";
+            isPlayerTurn = false;
+            UpdateBoard();
+            ComputerMove();
+        }
+    }
+
+    private void ComputerMove()
+    {
+        int? bestMove = FindBestMove();
+
+        if (bestMove.HasValue)
+        {
+            board[bestMove.Value] = "O";
+            isPlayerTurn = true;
+            UpdateBoard();
+        }
+    }
+
+    private int? FindBestMove()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (board[i] == null)
             {
-                img = new Image
+                board[i] = "O";
+                if (CheckWinner() == "O")
                 {
-
-                    HeightRequest = 125,
-                    Source = "blank.png"
-                };
-                WinOrLose[i, j] = 0;
-                var tap = new TapGestureRecognizer();
-                tap.Tapped += TapOnTapped;
-                field.Children.Add(img, i, j);
-                img.GestureRecognizers.Add(tap);
+                    board[i] = null;
+                    return i;
+                }
+                board[i] = null;
             }
         }
-    }
 
-    private void clearbtnOnClicked(object sender, EventArgs e)
-    {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 9; i++)
         {
-            for (int j = 0; j < 3; j++)
+            if (board[i] == null)
             {
-                img = new Image
+                board[i] = "X";
+                if (CheckWinner() == "X")
                 {
-                    HeightRequest = 125,
-                    Source = "blank.png"
-                };
-                var tap = new TapGestureRecognizer();
-                tap.Tapped += TapOnTapped;
-                field.Children.Add(img, i, j);
-                img.GestureRecognizers.Add(tap);
+                    board[i] = null;
+                    return i;
+                }
+                board[i] = null;
             }
         }
-        turnbtn.Opacity = 1;
-        player = 0;
-        victor = 0;
 
-        for (int y = 0; y < 3; y++)
+        if (board[4] == null)
         {
-            for (int x = 0; x < 3; x++)
+            return 4;
+        }
+
+        int[] corners = { 0, 2, 6, 8 };
+        foreach (var corner in corners)
+        {
+            if (board[corner] == null)
             {
-                WinOrLose[y, x] = 0;
+                return corner;
             }
         }
-    }
 
-    private void turnbtnOnClicked(object sender, EventArgs e)
-    {
-        Random rnd = new Random();
-        player = rnd.Next(1, 3);
-        if (player == 1)
+        for (int i = 0; i < 9; i++)
         {
-            DisplayAlert("Who's first?", "X Begins", "ОK");
-        }
-        else
-        {
-            DisplayAlert("Who's first?", "O Begins", "ОK");
-        }
-        turnbtn.Opacity = 0;
-
-    }
-
-    private void TapOnTapped(object sender, EventArgs e)
-    {
-        Image img = sender as Image;
-        var imageSource = (Image)sender;
-        var selectedImage = imageSource.Source as FileImageSource;
-
-        if (selectedImage == "blank.png")
-        {
-            if (player == 1)
+            if (board[i] == null)
             {
-                img.Source = ImageSource.FromFile("x.png");
-                WinOrLose[Grid.GetRow(img), Grid.GetColumn(img)] = 1;
-                player = 2;
-                VictoryCheck();
-                Winner();
+                return i;
             }
-            else if (player == 2)
+        }
+
+        return null;
+    }
+
+    private string CheckWinner()
+    {
+        string[,] winningCombinations = new string[,]
+        {
+                { "0", "1", "2" },
+                { "3", "4", "5" },
+                { "6", "7", "8" },
+                { "0", "3", "6" },
+                { "1", "4", "7" },
+                { "2", "5", "8" },
+                { "0", "4", "8" },
+                { "2", "4", "6" }
+        };
+
+        for (int i = 0; i < winningCombinations.GetLength(0); i++)
+        {
+            string a = board[int.Parse(winningCombinations[i, 0])];
+            string b = board[int.Parse(winningCombinations[i, 1])];
+            string c = board[int.Parse(winningCombinations[i, 2])];
+
+            if (a != null && a == b && a == c)
             {
-                img.Source = ImageSource.FromFile("o.png");
-                WinOrLose[Grid.GetRow(img), Grid.GetColumn(img)] = 2;
-                player = 1;
-                VictoryCheck();
-                Winner();
+                return a;
             }
         }
-        else
-        {
-            DisplayAlert("Error", "Occupied", "Ок");
-        }
-    }
-    public int VictoryCheck()
-    {
-        if (WinOrLose[0, 0] == 1 && WinOrLose[0, 1] == 1 && WinOrLose[0, 2] == 1)
-        {
-            victor = 1;
-        }
-        else if (WinOrLose[1, 0] == 1 && WinOrLose[1, 1] == 1 && WinOrLose[1, 2] == 1)
-        {
-            victor = 1;
-        }
-        else if (WinOrLose[2, 0] == 1 && WinOrLose[2, 1] == 1 && WinOrLose[2, 2] == 1)
-        {
-            victor = 1;
-        }
-        else if (WinOrLose[0, 0] == 1 && WinOrLose[1, 1] == 1 && WinOrLose[2, 2] == 1)
-        {
-            victor = 1;
-        }
-        else if (WinOrLose[0, 2] == 1 && WinOrLose[1, 1] == 1 && WinOrLose[2, 0] == 1)
-        {
-            victor = 1;
-        }
-        else if (WinOrLose[0, 0] == 1 && WinOrLose[1, 0] == 1 && WinOrLose[2, 0] == 1)
-        {
-            victor = 1;
-        }
-        else if (WinOrLose[0, 1] == 1 && WinOrLose[1, 1] == 1 && WinOrLose[2, 1] == 1)
-        {
-            victor = 1;
-        }
-        else if (WinOrLose[0, 2] == 1 && WinOrLose[1, 2] == 1 && WinOrLose[2, 2] == 1)
-        {
-            victor = 1;
-        }
 
-        else if (WinOrLose[0, 0] == 2 && WinOrLose[0, 1] == 2 && WinOrLose[0, 2] == 2)
-        {
-            victor = 2;
-        }
-        else if (WinOrLose[1, 0] == 2 && WinOrLose[1, 1] == 2 && WinOrLose[1, 2] == 2)
-        {
-            victor = 2;
-        }
-        else if (WinOrLose[2, 0] == 2 && WinOrLose[2, 1] == 2 && WinOrLose[2, 2] == 2)
-        {
-            victor = 2;
-        }
-        else if (WinOrLose[0, 0] == 2 && WinOrLose[1, 1] == 2 && WinOrLose[2, 2] == 2)
-        {
-            victor = 2;
-        }
-        else if (WinOrLose[0, 2] == 2 && WinOrLose[1, 1] == 2 && WinOrLose[2, 0] == 2)
-        {
-            victor = 2;
-        }
-        else if (WinOrLose[0, 0] == 2 && WinOrLose[1, 0] == 2 && WinOrLose[2, 0] == 2)
-        {
-            victor = 2;
-        }
-        else if (WinOrLose[0, 1] == 2 && WinOrLose[1, 1] == 2 && WinOrLose[2, 1] == 2)
-        {
-            victor = 2;
-        }
-        else if (WinOrLose[0, 2] == 2 && WinOrLose[1, 2] == 1 && WinOrLose[2, 2] == 2)
-        {
-            victor = 2;
-        }
-
-        return victor;
-    }
-    public void Winner()
-    {
-        victor = VictoryCheck();
-
-        if (victor == 1)
-        {
-            DisplayAlert("Victory!", "X Won", "ОK");
-        }
-        else if (victor == 2)
-        {
-            DisplayAlert("Victory!", "O Won", "ОK");
-        }
+        return board.All(b => b != null) ? "Draw" : null;
     }
 }

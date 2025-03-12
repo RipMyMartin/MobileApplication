4c8f761d4fdf300a719c52a15f9072e0b44da860
ripmymartin <martinsild.mr@gmail.com>
2025-03-12 11:38:47 +0200

    contact phone commit

--- FrendsContact.xaml.cs
+++ FrendsContact.xaml.cs
@@ -1,9 +1,41 @@
-namespace MobileApplication;
+using System;
+using System.Collections.Generic;
 
-public partial class FrendsContact : ContentPage
+namespace MobileApplication
 {
-	public FrendsContact()
-	{
-		InitializeComponent();
-	}
+    public partial class FrendsContact : ContentPage
+    {
+        public FrendsContact(int k)
+        {
+            InitializeComponent();
+        }
+
+        private async void SendSMS_Clicked(object sender, EventArgs e)
+        {
+            string phone = emailPhone.Text;
+            var message = "Tere tulemast! saadan sõnumi";
+            SmsMessage sms = new SmsMessage(message, phone);
+            if (phone != null && Sms.Default.IsComposeSupported)
+            {
+                await Sms.Default.ComposeAsync(sms);
+            }
+        }
+
+        private async void SendEmail_Clicked(object sender, EventArgs e)
+        {
+            var message = "Tere tulemast! saadan email";
+            EmailMessage eMail = new EmailMessage
+            {
+                Subject = emailPhone.Text,
+                Body = message,
+                BodyFormat = EmailBodyFormat.PlainText,
+                To = new List<string> { emailPhone.Text }
+            };
+
+            if (Email.Default.IsComposeSupported)
+            {
+                await Email.Default.ComposeAsync(eMail);
+            }
+        }
+    }
 }
